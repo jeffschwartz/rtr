c98210060572d50c41399e06a54159e97531473e
jeffschwartz <jefftschwartz@gmail.com>
2014-12-11 20:22:41 -0500

    Serializing form data using an OS library.

--- history-element.js
+++ history-element.js
@@ -1,7 +1,80 @@
 (function(w) {
     "use strict";
+
+    function serialize(form) {
+        if (!form || form.nodeName !== "FORM") {
+            return;
+        }
+        var i, j, q = [];
+        for (i = form.elements.length - 1; i >= 0; i = i - 1) {
+            if (form.elements[i].name === "") {
+                continue;
+            }
+            switch (form.elements[i].nodeName) {
+                case "INPUT":
+                    switch (form.elements[i].type) {
+                        case "text":
+                        case "hidden":
+                        case "password":
+                        case "button":
+                        case "reset":
+                        case "submit":
+                            q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[
+                                    i]
+                                .value));
+                            break;
+                        case "checkbox":
+                        case "radio":
+                            if (form.elements[i].checked) {
+                                q.push(form.elements[i].name + "=" + encodeURIComponent(
+                                    form.elements[
+                                        i].value));
+                            }
+                            break;
+                        case "file":
+                            break;
+                    }
+                    break;
+                case "TEXTAREA":
+                    q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i]
+                        .value));
+                    break;
+                case "SELECT":
+                    switch (form.elements[i].type) {
+                        case "select-one":
+                            q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[
+                                    i]
+                                .value));
+                            break;
+                        case "select-multiple":
+                            for (j = form.elements[i].options.length - 1; j >= 0; j = j - 1) {
+                                if (form.elements[i].options[j].selected) {
+                                    q.push(form.elements[i].name + "=" + encodeURIComponent(
+                                        form.elements[
+                                            i].options[j].value));
+                                }
+                            }
+                            break;
+                    }
+                    break;
+                case "BUTTON":
+                    switch (form.elements[i].type) {
+                        case "reset":
+                        case "submit":
+                        case "button":
+                            q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[
+                                    i]
+                                .value));
+                            break;
+                    }
+                    break;
+            }
+        }
+        return q.join("&");
+    }
+
     /**
-     * Creates a hash from an array whose elements are hashes whose properties are 'name' and 'value'.
+     * Creates a hash from an array whose elements are hashes whose properties are "name" and "value".
      */
     function valuesHashFromSerializedArray(valuesArray) {
         var valuesHash = {};
@@ -44,10 +117,12 @@
                 console.log("attribute method", method);
                 if (action.indexOf("/") === 0) {
                     evt.preventDefault();
-                    method = method ? method : 'get';
+                    method = method ? method : "get";
                     // valuesHash = valuesHashFromSerializedArray($form.serializeArray());
                     // FormData - see https://developer.mozilla.org/en-US/docs/Web/API/FormData for info
-                    valuesHash = valuesHashFromSerializedArray(new FormData(this));
+                    // var fd = new FormData(document.getElementById(evt.target.id));
+                    // console.log(fd);
+                    valuesHash = valuesHashFromSerializedArray(serialize(evt.target));
                     console.log("valuesHash", valuesHash);
                 }
             }

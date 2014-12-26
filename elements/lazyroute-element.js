6fe99a6349a31b377bd53686187470cbe7ff6a62
jeffschwartz <jefftschwartz@gmail.com>
2014-12-26 17:08:43 -0500

    Converted tabs to spaces (Atom editor is going nuts).

--- elements/lazyroute-element.js
+++ elements/lazyroute-element.js
@@ -1,27 +1,27 @@
 (function() {
-	"use strict";
-	Polymer("lazyroute-element", {
-		handler: "routeHandler",
-        ready: function () {
+    "use strict";
+    Polymer("lazyroute-element", {
+        handler: "routeHandler",
+        ready: function() {
             this.isImported = false;
         },
-		//Dynamically import element, flag it as loaded and call the handler
-		routeHandler: function() {
-			var self = this;
-			//Dynamically mport the element and flag it imported
-			if (!this.isImported) {
-				Polymer.import([this.importPath], function() {
-					var routerEl = document.querySelector("router-element"),
-						customEl = document.createElement(self.name);
-					self.isImported = true;
-					customEl.setAttribute("method", "{{method}}");
-					customEl.setAttribute("path", "{{path}}");
-					customEl.setAttribute("handler", "{{handler}}");
-					routerEl.appendChild(customEl);
+        //Dynamically import element, flag it as loaded and call the handler
+        routeHandler: function() {
+            var self = this;
+            //Dynamically mport the element and flag it imported
+            if (!this.isImported) {
+                Polymer.import([this.importPath], function() {
+                    var routerEl = document.querySelector("router-element"),
+                        customEl = document.createElement(self.name);
+                    self.isImported = true;
+                    customEl.setAttribute("method", "{{method}}");
+                    customEl.setAttribute("path", "{{path}}");
+                    customEl.setAttribute("handler", "{{handler}}");
+                    routerEl.appendChild(customEl);
                     // Polymer.register(customEl);
-				});
-			}
-			//Call the element's handler
-		}
-	});
+                });
+            }
+            //Call the element's handler
+        }
+    });
 }());

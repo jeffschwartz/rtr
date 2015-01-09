e0bf8d09ccd8313654b3c66bbab047534e547179
jeffschwartz <jefftschwartz@gmail.com>
2015-01-08 19:57:21 -0500

    Added more demo content.

---
+++ demo-redirect.js
@@ -0,0 +1,24 @@
+(function() {
+    Polymer("demo-redirect", {
+        domReady: function() {
+            this.super();
+            this.infoEl = document.getElementById("demo-redirect-info");
+        },
+        get: function(){
+            "use strict";
+            this.infoEl.textContent =  "demo-redirect \"get\" handler was called " +
+                "and is redirecting to /rtr/demo-redirecttarget";
+            console.log("demo-redirect get called");
+            this.rtrHistory.navigate({
+                state: {
+                    verb: "get",
+                    path: "/rtr/demo-redirecttarget"
+                },
+                method: "get",
+                url: "/rtr/demo-redirecttarget",
+                pushState: true,
+                trigger: true
+            });
+        }
+    });
+}());

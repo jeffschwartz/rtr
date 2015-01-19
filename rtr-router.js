06abe6fc04f893f9898c7f398fafd59856cff28e
jeffschwartz <jefftschwartz@gmail.com>
2015-01-19 11:14:06 -0500

    Trying to get @attributes to work with core-component-page.

--- rtr-router.js
+++ rtr-router.js
@@ -172,8 +172,7 @@ license that can be found in the LICENSE file.
     Polymer("rtr-router", {
 
         /**
-         * A lifecycle callback. Adds rtrHistory property to itself and propagates the routes array.
-         *
+         * @description A lifecycle callback. Adds rtrHistory property to itself and propagates the routes array.
          * @method domReady
          */
         domReady: function() {
